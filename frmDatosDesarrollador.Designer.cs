76de60cdfd23403aa4937e0d76a2e97a88a46e53
lucab <lucab@LUCA>
2023-05-11 22:26:15 -0300

    f1

--- frmDatosDesarrollador.Designer.cs
+++ frmDatosDesarrollador.Designer.cs
@@ -29,21 +29,167 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDatosDesarrollador));
+            this.lblComisionEsc = new System.Windows.Forms.Label();
+            this.lblComision = new System.Windows.Forms.Label();
+            this.lblFechaNum = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblNombrePry = new System.Windows.Forms.Label();
+            this.lblActividad = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblAlumno = new System.Windows.Forms.Label();
+            this.lblMateriaEsc = new System.Windows.Forms.Label();
+            this.lblMateria = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblComisionEsc
+            // 
+            this.lblComisionEsc.AutoSize = true;
+            this.lblComisionEsc.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblComisionEsc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComisionEsc.Location = new System.Drawing.Point(66, 26);
+            this.lblComisionEsc.Name = "lblComisionEsc";
+            this.lblComisionEsc.Size = new System.Drawing.Size(94, 18);
+            this.lblComisionEsc.TabIndex = 133;
+            this.lblComisionEsc.Text = "\"A\" INF - TM";
+            // 
+            // lblComision
+            // 
+            this.lblComision.AutoSize = true;
+            this.lblComision.Location = new System.Drawing.Point(12, 26);
+            this.lblComision.Name = "lblComision";
+            this.lblComision.Size = new System.Drawing.Size(49, 13);
+            this.lblComision.TabIndex = 132;
+            this.lblComision.Text = "Comision";
+            // 
+            // lblFechaNum
+            // 
+            this.lblFechaNum.AutoSize = true;
+            this.lblFechaNum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFechaNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaNum.Location = new System.Drawing.Point(64, 98);
+            this.lblFechaNum.Name = "lblFechaNum";
+            this.lblFechaNum.Size = new System.Drawing.Size(75, 18);
+            this.lblFechaNum.TabIndex = 130;
+            this.lblFechaNum.Text = "12/5/2023";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(12, 99);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 129;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // lblNombrePry
+            // 
+            this.lblNombrePry.AutoSize = true;
+            this.lblNombrePry.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNombrePry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombrePry.Location = new System.Drawing.Point(64, 78);
+            this.lblNombrePry.Name = "lblNombrePry";
+            this.lblNombrePry.Size = new System.Drawing.Size(14, 18);
+            this.lblNombrePry.TabIndex = 128;
+            this.lblNombrePry.Text = "-";
+            // 
+            // lblActividad
+            // 
+            this.lblActividad.AutoSize = true;
+            this.lblActividad.Location = new System.Drawing.Point(12, 79);
+            this.lblActividad.Name = "lblActividad";
+            this.lblActividad.Size = new System.Drawing.Size(54, 13);
+            this.lblActividad.TabIndex = 127;
+            this.lblActividad.Text = "Actividad:";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(66, 61);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(73, 18);
+            this.lblNumero.TabIndex = 126;
+            this.lblNumero.Text = "45748642";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(66, 42);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(114, 18);
+            this.lblNombre.TabIndex = 125;
+            this.lblNombre.Text = "Bonadero Luca";
+            // 
+            // lblAlumno
+            // 
+            this.lblAlumno.AutoSize = true;
+            this.lblAlumno.Location = new System.Drawing.Point(12, 43);
+            this.lblAlumno.Name = "lblAlumno";
+            this.lblAlumno.Size = new System.Drawing.Size(45, 13);
+            this.lblAlumno.TabIndex = 124;
+            this.lblAlumno.Text = "Alumno:";
+            // 
+            // lblMateriaEsc
+            // 
+            this.lblMateriaEsc.AutoSize = true;
+            this.lblMateriaEsc.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMateriaEsc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMateriaEsc.Location = new System.Drawing.Point(66, 10);
+            this.lblMateriaEsc.Name = "lblMateriaEsc";
+            this.lblMateriaEsc.Size = new System.Drawing.Size(145, 18);
+            this.lblMateriaEsc.TabIndex = 123;
+            this.lblMateriaEsc.Text = "Estructura de Datos";
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Location = new System.Drawing.Point(12, 9);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(48, 13);
+            this.lblMateria.TabIndex = 122;
+            this.lblMateria.Text = "Materia: ";
             // 
             // frmDatosDesarrollador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 328);
+            this.ClientSize = new System.Drawing.Size(334, 134);
+            this.Controls.Add(this.lblComisionEsc);
+            this.Controls.Add(this.lblComision);
+            this.Controls.Add(this.lblFechaNum);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblNombrePry);
+            this.Controls.Add(this.lblActividad);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblAlumno);
+            this.Controls.Add(this.lblMateriaEsc);
+            this.Controls.Add(this.lblMateria);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmDatosDesarrollador";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Datos del Desarrollador";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblComisionEsc;
+        private System.Windows.Forms.Label lblComision;
+        private System.Windows.Forms.Label lblFechaNum;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblNombrePry;
+        private System.Windows.Forms.Label lblActividad;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblAlumno;
+        private System.Windows.Forms.Label lblMateriaEsc;
+        private System.Windows.Forms.Label lblMateria;
     }
 }
